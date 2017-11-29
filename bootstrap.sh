f8c28728600c02438c351a211134b9ce4afbcd0a
Charles <crossan007@gmail.com>
2017-11-28 21:29:45 -0500

    install composer for system from source

--- bootstrap.sh
+++ bootstrap.sh
@@ -83,8 +83,10 @@ echo "=========================================================="
 echo "=================   Composer Update    ==================="
 echo "=========================================================="
 
-sudo apt-get -qq install -y composer
-sudo /usr/local/bin/composer self-update
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
 
 echo "=========================================================="
 echo "=================   Node Update    ==================="
